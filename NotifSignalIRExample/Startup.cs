921dcb7964a2ff6dd90ca367748af9309dc07b44
David Zambrano <davazamb10@outlook.com>
2016-12-18 11:11:01 -0400

    Creados clases OWIN, SignalIR, componentes de clase de depeendencias sql, events, registros de notificacion de contactos

---
+++ NotifSignalIRExample/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(NotifSignalIRExample.Startup))]
+
+namespace NotifSignalIRExample
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Para obtener más información acerca de cómo configurar su aplicación, visite http://go.microsoft.com/fwlink/?LinkID=316888
+            app.MapSignalR();
+        }
+    }
+}
